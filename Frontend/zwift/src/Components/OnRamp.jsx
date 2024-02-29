1aea4cc343c92654d8b581fba19c70bc836db7ed
SoerenGG <soeren.gg@gmx.de>
2024-02-29 17:22:56 +0100

    Showing only response when successfull transaction, backbutton not implemented yet

--- Frontend/zwift/src/Components/OnRamp.jsx
+++ Frontend/zwift/src/Components/OnRamp.jsx
@@ -6,6 +6,7 @@ import {
   Typography,
   Divider,
   useTheme,
+  Button,
   Box,
 } from "@mui/material";
 import PayPalIntegration from "./PayPalIntegration";
@@ -15,6 +16,8 @@ const OnRamp = () => {
   const [amount, setAmount] = useState(0);
   const [email, setEmail] = useState("sb-sdcta29428430@personal.example.com");
   const theme = useTheme();
+  const [successfullResponse, setResponse] = useState(false);
+  const [doReset, setDoReset] = useState(false); // State to control reset
 
   const handleAmountChange = (event) => {
     const value = event.target.value;
@@ -22,58 +25,87 @@ const OnRamp = () => {
     setAmount(sanitizedValue);
   };
 
+  function handleResponseChange(newValue) {
+    setResponse(newValue);
+  }
+
+
   return (
-    <Box sx={{ maxWidth: 500, mx: "auto", mt: 5 }}>
-      {/* Headline with icon */}
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "left",
-          bgcolor: "blue",
-          color: "white",
-          p: 2,
-          borderRadius: "4px 4px 0 0", // Optional: rounds the top corners
-        }}
-      >
-        <SettingsIcon sx={{ mr: 1 }} />
-        <Typography variant="h6">Set Details</Typography>
-      </Box>
+    <div>
+      {!successfullResponse && (
+        <Box sx={{ maxWidth: 500, mx: "auto", mt: 5 }}>
+          {/* Headline with icon */}
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "left",
+              bgcolor: "blue",
+              color: "white",
+              p: 2,
+              borderRadius: "4px 4px 0 0", // Optional: rounds the top corners
+            }}
+          >
+            <SettingsIcon sx={{ mr: 1 }} />
+            <Typography variant="h6">Set Details</Typography>
+          </Box>
+
+          <Box sx={{ maxWidth: 500, mx: "auto", mt: 5 }}>
+            <Card sx={{ padding: 4, mb: 2 }}>
+              <Stack spacing={2} alignItems="flex-start">
+                <Typography
+                  variant="h5"
+                  gutterBottom
+                  sx={{ fontWeight: "light" }}
+                >
+                  Set the amount to onramp:
+                </Typography>
+                <TextField
+                  variant="outlined"
+                  type="text"
+                  value={amount}
+                  onChange={handleAmountChange}
+                  fullWidth
+                />
+                <Divider
+                  sx={{ width: "100%", my: 2, borderColor: "primary.main" }}
+                />{" "}
+                {/* Spacing & Divider */}
+                <Typography
+                  variant="h5"
+                  gutterBottom
+                  sx={{ fontWeight: "light" }}
+                >
+                  Peer to exchange with:
+                </Typography>
+                <TextField
+                  disabled
+                  type="email"
+                  variant="outlined"
+                  value={email}
+                  fullWidth
+                />
+              </Stack>
+            </Card>
+          </Box>
+        </Box>
+      )}
 
       <Box sx={{ maxWidth: 500, mx: "auto", mt: 5 }}>
-        <Card sx={{ padding: 4, mb: 2 }}>
-          <Stack spacing={2} alignItems="flex-start">
-            <Typography variant="h5" gutterBottom sx={{ fontWeight: "light" }}>
-              Set the amount to onramp:
-            </Typography>
-            <TextField
-              variant="outlined"
-              type="text"
-              value={amount}
-              onChange={handleAmountChange}
-              fullWidth
-            />
-            <Divider
-              sx={{ width: "100%", my: 2, borderColor: "primary.main" }}
-            />{" "}
-            {/* Spacing & Divider */}
-            <Typography variant="h5" gutterBottom sx={{ fontWeight: "light" }}>
-              Peer to exchange with:
-            </Typography>
-            <TextField
-              disabled
-              type="email"
-              variant="outlined"
-              value={email}
-              fullWidth
-            />
-          </Stack>
-        </Card>
-        <Box sx={{ mt: 5 }}>
-          <PayPalIntegration amount={amount} email={email} />
-        </Box>
+        <Stack direction="column">
+          {successfullResponse && (
+            <Button variant="outlined">
+              Perform another OnRamp Transaction
+            </Button>
+          )}
+          <PayPalIntegration
+            amount={amount}
+            email={email}
+            onSuccessfullResponse={handleResponseChange}
+          />
+        </Stack>
       </Box>
-    </Box>
+    </div>
   );
 };
 
