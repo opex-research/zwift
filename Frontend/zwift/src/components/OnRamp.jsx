df50b57d7d62f4c4374aa8016c1d199fee3a5ef2
Michel Osswald <ga27caf@mytum.de>
2024-03-07 09:31:56 +0100

    - resizable window

--- Frontend/zwift/src/components/OnRamp.jsx
+++ Frontend/zwift/src/components/OnRamp.jsx
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
 import {
   TextField,
-  Stack,
-  Card,
   Typography,
   Divider,
   useTheme,
   Button,
   Box,
+  Paper,
+  Grid,
 } from "@mui/material";
 import PayPalIntegration from "./PayPalIntegration";
-import SettingsIcon from "@mui/icons-material/Settings"; // Import Settings icon
+import CashIcon from "../icons/icons8-cashflow-48.png"; // Import the PNG file
+import ExchangeIcon from "../icons/icons8-transfer-zwischen-benutzern-48.png"; // Import the PNG file
 
 const OnRamp = () => {
   const [amount, setAmount] = useState(0);
   const [email, setEmail] = useState("sb-sdcta29428430@personal.example.com");
   const [successfullResponse, setResponse] = useState(false);
-  const [doReset, setDoReset] = useState(false); // State to control reset
   const [resetCounter, setResetCounter] = useState(0);
-
-  //Here we initate the PayPal states to reset them when needed
   const [paymentDetails, setPaymentDetails] = useState(null);
 
   const handleAmountChange = (event) => {
@@ -28,95 +26,105 @@ const OnRamp = () => {
     setAmount(sanitizedValue);
   };
 
-  function handleResponseChange(newValue) {
+  const handleResponseChange = (newValue) => {
     setResponse(newValue);
-  }
+  };
 
-  function handleBackButton() {
+  const handleBackButton = () => {
     setAmount(0);
     setPaymentDetails(null);
-    setResponse(false); // Reset to allow another transaction
+    setResponse(false);
     setResetCounter((prev) => prev + 1);
-  }
+  };
+
+  const theme = useTheme();
 
   return (
-    <div>
-      {!successfullResponse && (
-        <Box sx={{ maxWidth: 500, mx: "auto", mt: 5 }}>
-          {/* Headline with icon */}
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "left",
-              bgcolor: "blue",
-              color: "white",
-              p: 2,
-              borderRadius: "4px 4px 0 0", // Optional: rounds the top corners
-            }}
-          >
-            <SettingsIcon sx={{ mr: 1 }} />
-            <Typography variant="h6">Set Details</Typography>
-          </Box>
-
-          <Box sx={{ maxWidth: 500, mx: "auto", mt: 5 }}>
-            <Card sx={{ padding: 4, mb: 2 }}>
-              <Stack spacing={2} alignItems="flex-start">
-                <Typography
-                  variant="h5"
-                  gutterBottom
-                  sx={{ fontWeight: "light" }}
-                >
-                  Set the amount to onramp:
-                </Typography>
-                <TextField
-                  variant="outlined"
-                  type="text"
-                  value={amount}
-                  onChange={handleAmountChange}
-                  fullWidth
-                />
-                <Divider
-                  sx={{ width: "100%", my: 2, borderColor: "primary.main" }}
-                />{" "}
-                {/* Spacing & Divider */}
-                <Typography
-                  variant="h5"
-                  gutterBottom
-                  sx={{ fontWeight: "light" }}
-                >
-                  Peer to exchange with:
-                </Typography>
-                <TextField
-                  disabled
-                  type="email"
-                  variant="outlined"
-                  value={email}
-                  fullWidth
-                />
-              </Stack>
-            </Card>
-          </Box>
-        </Box>
-      )}
-
-      <Box sx={{ maxWidth: 500, mx: "auto", mt: 5 }}>
-        <Stack direction="column">
-          {successfullResponse && (
-            <Button variant="outlined" onClick={handleBackButton}>
-              Perform another OnRamp Transaction
-            </Button>
-          )}
-          <PayPalIntegration
-            amount={amount}
-            email={email}
-            onSuccessfullResponse={handleResponseChange}
-            paymentDetails={paymentDetails}
-            key={resetCounter} //we do this because a key forces a rerender when it changes
+    <Paper
+      elevation={1}
+      sx={{
+        p: theme.spacing(4),
+        maxWidth: 500,
+        mx: "auto",
+        my: theme.spacing(3),
+      }}
+    >
+      <Typography
+        variant="h4"
+        component="h1"
+        sx={{ fontWeight: "medium", marginBottom: 8 }}
+      >
+        On Ramp
+      </Typography>
+      <Grid container spacing={2} direction="column">
+        <Grid item container alignItems="flex-start">
+          <Grid item>
+            <img
+              src={CashIcon}
+              alt="Cash Icon"
+              style={{ width: "50%", height: "50%" }}
+            />
+          </Grid>
+          <Grid item xs>
+            <Typography variant="caption" display="block" color="textSecondary">
+              SET AMOUNT TO ONRAMP
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <TextField
+            variant="outlined"
+            type="text"
+            value={amount}
+            onChange={handleAmountChange}
+            fullWidth
           />
-        </Stack>
-      </Box>
-    </div>
+        </Grid>
+        <Grid item>
+          <Divider sx={{ marginY: theme.spacing(2) }} />
+        </Grid>
+        <Grid item container alignItems="flex-start">
+          <Grid item>
+            <img
+              src={ExchangeIcon}
+              alt="Exchange Icon"
+              style={{ width: "50%", height: "50%" }}
+            />
+          </Grid>
+          <Grid item xs>
+            <Typography variant="caption" display="block" color="textSecondary">
+              PEER TO EXCHANGE WITH
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <TextField
+            disabled
+            type="email"
+            variant="outlined"
+            value={email}
+            fullWidth
+          />
+        </Grid>
+
+        <Box sx={{ maxWidth: 500, mx: "auto", mt: 5 }}>
+          <Grid container direction="column">
+            {successfullResponse && (
+              <Button variant="outlined" onClick={handleBackButton}>
+                Perform another OnRamp Transaction
+              </Button>
+            )}
+            <PayPalIntegration
+              amount={amount}
+              email={email}
+              onSuccessfullResponse={handleResponseChange}
+              paymentDetails={paymentDetails}
+              key={resetCounter}
+            />
+          </Grid>
+        </Box>
+      </Grid>
+    </Paper>
   );
 };
 
--- Frontend/zwift/src/components/UserAccount.jsx
+++ Frontend/zwift/src/components/UserAccount.jsx
@@ -1,97 +1,153 @@
 import React from "react";
-import { Button, Typography, Stack } from "@mui/material";
-import { useAccount } from "../context/AccountContext"; // Adjust the path as necessary
+import {
+  Button,
+  Typography,
+  Grid,
+  Paper,
+  Divider,
+  useTheme,
+} from "@mui/material";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import EmailIcon from "@mui/icons-material/Email";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useAccount } from "../context/AccountContext";
 import { useNavigate } from "react-router-dom";
+import WalletIcon from "../icons/icons8-brieftasche-48.png"; // Import the PNG file
+import MailIcon from "../icons/icons8-neuer-beitrag-48.png"; // Import the PNG file
+import CashIcon from "../icons/icons8-münzen-48.png"; // Import the PNG file
 
 const UserAccount = () => {
-  const { account, balance, registeredEmail, setLogged, setAccount, setBalance, setRegisteredEmail } = useAccount();
+  const {
+    account,
+    balance,
+    registeredEmail,
+    setLogged,
+    setAccount,
+    setBalance,
+    setRegisteredEmail,
+  } = useAccount();
   const navigate = useNavigate();
-
-  let walletAddress = account;
-  let walletBalance = balance;
-  let offRampAmount = "20Eth";
-  let registeredEmailAddress = registeredEmail
+  const theme = useTheme();
 
   const handleLogout = () => {
     setLogged(false);
     setAccount(null);
     setBalance();
+    setRegisteredEmail("");
     navigate("/");
   };
 
   return (
-    <Stack
-      direction="column"
-      spacing={2}
-      alignItems="center"
-      style={{ padding: 16 }}
+    <Paper
+      elevation={1}
+      sx={{
+        p: theme.spacing(4),
+        maxWidth: 500,
+        mx: "auto",
+        my: theme.spacing(3),
+      }}
     >
-      {/* "Sören" and "Logout" */}
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        width="100%"
-        spacing={2}
+      <Grid
+        container
         alignItems="center"
+        justifyContent="space-between" // Align items and button to opposite ends
+        sx={{ marginBottom: theme.spacing(2) }} // Reduce bottom margin for the grid
       >
-        <Typography variant="h4">Sören</Typography>
-        <Button variant="text" onClick={handleLogout} sx={{ padding: 0 }}>
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{
+            fontWeight: "medium",
+            marginBottom: 8, // Remove bottom margin for the title
+          }}
+        >
+          Account Details
+        </Typography>
+        <Button
+          color="primary"
+          onClick={handleLogout}
+          sx={{
+            fontSize: theme.typography.body1.fontSize,
+            fontWeight: "ultralight", // Ultra-light font weight
+            marginBottom: 8, // Remove bottom margin for the title
+            color: theme.palette.primary.main, // Primary color
+          }}
+        >
           Logout
         </Button>
-      </Stack>
-      {/* Wallet Address */}
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        width="100%"
-        spacing={2}
-        alignItems="center"
-      >
-        <Typography variant="h6" component="div">
-          Wallet Address:
-        </Typography>
-        <Typography component="div">{walletAddress}</Typography>
-      </Stack>
-      {/* Wallet Balance */}
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        width="100%"
-        spacing={2}
-        alignItems="center"
-      >
-        <Typography variant="h6" component="div">
-          Wallet Balance:
-        </Typography>
+      </Grid>
 
-        <Typography component="div">{walletBalance}</Typography>
-      </Stack>
-      {/* Open OffRamp */}
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        width="100%"
-        spacing={2}
+      <Grid
+        container
         alignItems="center"
-      >
-        <Typography variant="h6" component="div">
-          Open OffRamp:
-        </Typography>
-        <Typography component="div">{offRampAmount}</Typography>
-      </Stack>
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        width="100%"
         spacing={2}
-        alignItems="center"
+        sx={{ marginBottom: theme.spacing(1) }}
       >
-        <Typography variant="h6" component="div">
-          Registered Email:
-        </Typography>
-        <Typography component="div">{registeredEmailAddress}</Typography>
-      </Stack>
-    </Stack>
+        <Grid item>
+          <img
+            src={WalletIcon}
+            alt="Wallet Icon"
+            style={{ width: "50%", height: "50%" }}
+          />
+        </Grid>
+        <Grid item xs>
+          <Typography variant="caption" display="block" color="textSecondary">
+            WALLET ADDRESS
+          </Typography>
+          <Typography variant="body1" color="textPrimary">
+            {account}
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Divider sx={{ marginY: theme.spacing(4) }} />
+      <Grid
+        container
+        alignItems="center"
+        spacing={2}
+        sx={{ marginBottom: theme.spacing(1) }}
+      >
+        <Grid item>
+          <img
+            src={MailIcon}
+            alt="Mail Icon"
+            style={{ width: "50%", height: "50%" }}
+          />
+        </Grid>
+        <Grid item xs>
+          <Typography variant="caption" display="block" color="textSecondary">
+            REGISTERED EMAIL
+          </Typography>
+          <Typography variant="body1" color="textPrimary">
+            {registeredEmail}
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Divider sx={{ marginY: theme.spacing(4) }} />
+      <Grid
+        container
+        alignItems="center"
+        spacing={2}
+        sx={{ marginBottom: theme.spacing(1) }}
+      >
+        <Grid item>
+          <img
+            src={CashIcon}
+            alt="Cash Icon"
+            style={{ width: "50%", height: "50%" }}
+          />
+        </Grid>
+        <Grid item xs>
+          <Typography variant="caption" display="block" color="textSecondary">
+            WALLET BALANCE
+          </Typography>
+          <Typography variant="body1" color="textPrimary">
+            {balance}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
