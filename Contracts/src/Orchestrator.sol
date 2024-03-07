1f7bb6532cd6ac19e40ae87a5872643c15b08351
SoerenGG <soeren.gg@gmx.de>
2024-03-07 14:54:22 +0100

    implemented Peerfinder Contract and added to orchestrator

--- Contracts/src/Orchestrator.sol
+++ Contracts/src/Orchestrator.sol
@@ -13,15 +13,27 @@ interface IOffRamper {
     function getOpenOffRampIntent(address wallet) external view returns (uint16);
 }
 
+interface IPeerFinder {
+    function addOffRampersIntent(address _address) external;
+    function getAndRemoveOffRampersIntent() external returns (address);
+    function reinsertOffRampIntentAfterFailedOnRamp(address _address) external;
+    function peek() external view returns (address);
+    function isEmpty() external view returns (bool);
+    function size() external view returns (uint256);
+}
+
+
 
 contract Orchestrator {
     
     IRegistrator public registrator;
     IOffRamper public offRamper;
+    IPeerFinder public peerFinder;
 
-    constructor(address _registratorAddress, address _offRamperAddress) {
+    constructor(address _registratorAddress, address _offRamperAddress, address _peerFinderAddress) {
         registrator = IRegistrator(_registratorAddress);
-        offRamper = IOffRamper(_offRamperAddress); // Initialize offRamper
+        offRamper = IOffRamper(_offRamperAddress); 
+        peerFinder = IPeerFinder(_peerFinderAddress);
     }
 
     //------------------- Functions for IRegistrator -------------------
@@ -42,11 +54,44 @@ contract Orchestrator {
 
 
 
+    //------------------ Functions for IPeerFinder ----------------------
+    function addOffRampersIntentToQueue(address _address) internal {
+        peerFinder.addOffRampersIntent(_address);
+    }
+
+    function getAndRemoveOffRampersIntentFromQueue() external returns (address) {
+        return peerFinder.getAndRemoveOffRampersIntent();
+    }
+
+    function reinsertOffRampIntentAfterFailedOnRamp(address _address) external {
+        peerFinder.reinsertOffRampIntentAfterFailedOnRamp(_address);
+    }
+
+    function peekOffRamperQueue() external view returns (address) {
+        return peerFinder.peek();
+    }
+
+    function isOffRamperQueueEmpty() external view returns (bool) {
+        return peerFinder.isEmpty();
+    }
+
+    function numberOfOpenOffRampIntents() external view returns (uint256) {
+        return peerFinder.size();
+    }
+
+
+
     //------------------ Functions for IOffRamper ----------------------
     // Function to store an OffRamp Intent with a certain amount through the offRamper
     function newOffRampIntent(address wallet, uint16 amount) external returns (bool) {
-        return offRamper.newOffRampIntent(wallet, amount);
+        // Require that the new off-ramp intent is successful
+        require(offRamper.newOffRampIntent(wallet, amount), "OffRampIntent failed");
+        // Since the off-ramp intent was successful, add the wallet to the off-rampers intent queue
+        addOffRampersIntentToQueue(wallet);
+        // Return true to indicate success
+        return true;
     }
+
 
     // Function to decrease OffRampIntent after a successful OnRamp transaction through the offRamper
     function decreaseOffRampIntentAfterTransaction(address wallet, uint16 amount) external returns (bool) {
@@ -57,4 +102,7 @@ contract Orchestrator {
     function getOpenOffRampIntent(address wallet) external view returns (uint16) {
         return offRamper.getOpenOffRampIntent(wallet);
     }
+
+
+    
 }
--- Contracts/src/PeerFinder.sol
+++ Contracts/src/PeerFinder.sol
@@ -4,35 +4,44 @@ pragma solidity ^0.8.0;
 import "../lib/openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
 import "../lib/openzeppelin-contracts/contracts/utils/structs/DoubleEndedQueue.sol";
 
-contract AddressQueue {
+contract PeerFinder {
     using DoubleEndedQueue for DoubleEndedQueue.Bytes32;
 
-    DoubleEndedQueue.Bytes32 private queue;
+    DoubleEndedQueue.Bytes32 private offRamperQueue;
 
     // Enqueue an address to the back of the queue
-    function enqueue(address _address) public {
-        queue.pushBack(bytes32(uint256(uint160(_address))));
+    function addOffRampersIntent(address _address) public {
+        // Ensure the address is not zero
+        require(_address != address(0), "Invalid address");
+        offRamperQueue.pushBack(bytes32(uint256(uint160(_address))));
     }
 
-    // Dequeue an address from the front of the queue
-    function dequeue() public returns (address) {
-        require(!queue.isEmpty(), "Queue is empty");
-        return address(uint160(uint256(queue.popFront())));
+    // Dequeue an address from the front of the queue, needs to be added to the front if failed
+    function getAndRemoveOffRampersIntent() public returns (address) {
+        require(!offRamperQueue.isEmpty(), "Queue is empty");
+        return address(uint160(uint256(offRamperQueue.popFront())));
     }
+
+    // Reinsert offRamp intent to front of queue if onRamp did not work
+    function reinsertOffRampIntentAfterFailedOnRamp(address _address) public {
+        // Ensure the address is not zero
+        require(_address != address(0), "Invalid address");
+        offRamperQueue.pushFront(bytes32(uint256(uint160(_address))));
+    } 
 
     // Peek at the front address of the queue without removing it
     function peek() public view returns (address) {
-        require(!queue.isEmpty(), "Queue is empty");
-        return address(uint160(uint256(queue.front())));
+        require(!offRamperQueue.isEmpty(), "Queue is empty");
+        return address(uint160(uint256(offRamperQueue.front())));
     }
 
     // Check if the queue is empty
     function isEmpty() public view returns (bool) {
-        return queue.isEmpty();
+        return offRamperQueue.isEmpty();
     }
 
     // Get the size of the queue
     function size() public view returns (uint256) {
-        return queue.length();
+        return offRamperQueue.length();
     }
 }
