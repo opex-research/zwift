deb4e14cf75683db0648cd45e6ed36afc08dab8d
SoerenGG <soeren.gg@gmx.de>
2024-04-11 13:49:33 +0200

    added GET registration status in api

--- Backend/Database/main.py
+++ Backend/Database/main.py
@@ -21,6 +21,43 @@ app.add_middleware(
 )
 
 
+@app.get("/transactions/{wallet_address}/registrationstatus")
+def get_register_status(wallet_address: str):
+    conn = database.get_db_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute(
+            "SELECT * FROM transactions WHERE wallet_address = %s AND transaction_type = 'register'",
+            (wallet_address,),
+        )
+        transactions = cur.fetchall()
+        
+        # Check if there are no transactions first
+        if not transactions:
+            return {"registration_status": "not_registered"}
+        
+        # Now, it's safe to assume transactions is not empty
+        if len(transactions) > 1:
+            raise HTTPException(
+                status_code=404,
+                detail="Duplicate entries for registration transaction, contact support",
+            )
+        
+        # Assuming you're using RealDictCursor or similar to access columns by name
+        if transactions[0]['transaction_status'] == "success":
+            return {"registration_status": "registered"}
+        elif transactions[0]['transaction_status'] == "pending":
+            return {"registration_status": "pending"}
+        else:
+            return {"registration_status": "not_registered"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        cur.close()
+        conn.close()
+
+
+
 @app.get("/transactions/{wallet_address}/pending")
 def get_pending_transactions(wallet_address: str):
     conn = database.get_db_connection()
@@ -60,6 +97,7 @@ def create_transaction(transaction: TransactionBase):
     conn = database.get_db_connection()
     cur = conn.cursor()
     try:
+        print("trying to write transaction into databse")
         cur.execute(
             "INSERT INTO transactions (wallet_address, transaction_hash, transaction_type, transaction_status) VALUES (%s, %s, %s, %s) RETURNING *;",
             (
@@ -69,13 +107,15 @@ def create_transaction(transaction: TransactionBase):
                 transaction.transaction_status,
             ),
         )
+
         new_transaction = cur.fetchone()
+        print(new_transaction)
         conn.commit()
         return TransactionBase(
-            wallet_address=new_transaction[1],
-            transaction_hash=new_transaction[2],
-            transaction_type=new_transaction[3],
-            transaction_status=new_transaction[4],
+            wallet_address=new_transaction["wallet_address"],
+            transaction_hash=new_transaction["transaction_hash"],
+            transaction_type=new_transaction["transaction_type"],
+            transaction_status=new_transaction["transaction_status"],
         )
     except Exception as e:
         conn.rollback()
