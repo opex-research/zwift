f5b37566e83d8cfb159e460ead29a35f6dccdb3c
Michel Osswald <ga27caf@mytum.de>
2024-04-29 15:41:26 +0200

    - follow up fix

--- Backend/server.go
+++ Backend/server.go
@@ -12,7 +12,7 @@ import (
 	"github.com/opex-research/zwift/router"
 )
 
-var store = session.New() // Initialize a session store
+var store *session.Store
 
 func init() {
 	// Load the .env file from the current directory
@@ -32,42 +32,20 @@ func init() {
 
 func main() {
 	app := fiber.New(fiber.Config{
-		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
-			// Log the detailed error for internal diagnostics.
-			// This is logged first to ensure it appears right after Fiber's own log line for the request.
-			log.Printf("An error occurred: %v", err)
-
-			// Determine if it's a Fiber error with a custom status code and message.
-			e, ok := err.(*fiber.Error)
-			if !ok {
-				// For non-Fiber errors, assume it's an internal server error.
-				e = fiber.NewError(fiber.StatusInternalServerError, "Internal Server Error")
-			}
-
-			// Here, we log the error again only if you need to ensure the error specifics appear in the log,
-			// but typically, the first log statement is sufficient for internal diagnostics.
-
-			// Use a generic error message for the frontend, while preserving the original status code.
-			genericMessage := "An unexpected error occurred, please try again later."
-			if e.Code == fiber.StatusBadRequest {
-				// Assume BadRequest errors are safe and informative enough to send back as is.
-				genericMessage = e.Message
-			}
-
-			// Respond to the client with a generic message.
-			// This response is crafted to ensure that detailed error information is not exposed to the client.
-			return ctx.Status(e.Code).SendString(genericMessage)
-		},
+		ErrorHandler: customErrorHandler,
 	})
 
-	app.Use(logger.New(logger.Config{
-		// Configure logger if needed, the default configuration should automatically log before error handling.
-	}))
+	app.Use(logger.New())
 	app.Use(recover.New())
 
-	// Setup routes
+	// Setup routes with the configured session store
 	router.SetupRoutes(app, store)
 
 	// Start the server
 	log.Fatal(app.Listen(":3001"))
+}
+
+func customErrorHandler(ctx *fiber.Ctx, err error) error {
+	// Custom error handling logic
+	return ctx.Status(500).SendString("Internal Server Error")
 }
