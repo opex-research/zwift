326f21136a17f4771a25b3c0d139b252983784e9
Michel Osswald <ga27caf@mytum.de>
2024-04-30 13:28:26 +0200

    - fix

--- Backend/Database/database.py
+++ Backend/Database/database.py
@@ -1,11 +1,10 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import os
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
-
-# Connection parameters
 DATABASE_URL = f"postgres://root@{DB_HOST}:26257/state_database"
+print("Database Host:", DB_HOST)
 
 
 def get_db_connection():
