823ed05143622f645da2d06fbb5d4e8eac8a8a5c
Michel Osswald <ga27caf@mytum.de>
2024-04-30 13:31:56 +0200

    - fix

--- Backend/Database/database.py
+++ Backend/Database/database.py
@@ -1,12 +1,19 @@
-import os
+import time
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DATABASE_URL = f"postgres://root@{DB_HOST}:26257/state_database"
-print("Database Host:", DB_HOST)
-
 
 def get_db_connection():
-    conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
-    return conn
+    attempts = 0
+    while attempts < 5:
+        try:
+            conn = psycopg2.connect(
+                dsn=f"postgres://root@{os.getenv('DB_HOST', 'localhost')}:26257/state_database",
+                cursor_factory=RealDictCursor,
+            )
+            return conn
+        except psycopg2.OperationalError as e:
+            print(f"Failed to connect to the database, retrying... ({attempts + 1})")
+            time.sleep(5)  # wait for 5 seconds before retrying
+            attempts += 1
+    raise Exception("Failed to connect to the database after several attempts")
