7808fdfb998b02d238f8b4493b651d007195e169
Michel Osswald <ga27caf@mytum.de>
2024-04-30 14:32:38 +0200

    - fix

--- Frontend/zwift/src/contracts/config.jsx
+++ Frontend/zwift/src/contracts/config.jsx
@@ -1,8 +1,8 @@
 // Import the ABI definitions from JSON files
 import OrchestratorABI from "../contracts/Orchestrator.json"; // Correct the path as needed
 import OrchestratorABIZksync from "../contracts/Orchestrator_zksync.json";
-import { Provider } from "zksync-ethers";
-import { ethers } from "ethers";
+import { Provider } from "zksync-ethers/lib/provider";
+import { providers } from "ethers/providers";
 // Define the contract addresses
 const orchestratorAddressAnvil = "0x95bD8D42f30351685e96C62EDdc0d0613bf9a87A";
 const orchestratorAddressZksync = "0x6f74F759715DFA0f323af0e2326E787D599FD42a";
@@ -18,7 +18,7 @@ const orchestratorAddress = isLocal
   ? orchestratorAddressAnvil
   : orchestratorAddressZksync;
 const provider = isLocal
-  ? new ethers.providers.Web3Provider(window.ethereum)
+  ? new providers.Web3Provider(window.ethereum)
   : new Provider("https://sepolia.era.zksync.dev");
 
 // Export the determined settings
