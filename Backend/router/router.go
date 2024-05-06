d0d13ff600e497c1d9b65c108889e85b07c40f3f
Michel Osswald <ga27caf@mytum.de>
2024-05-06 14:32:40 +0200

    - fix

--- Backend/router/router.go
+++ Backend/router/router.go
@@ -9,12 +9,12 @@ import (
 
 // SetupRoutes func
 func SetupRoutes(app *fiber.App, store *session.Store) {
-
 	h := &handlers.Handler{Store: store}
 
-	// Allow Cors from certain IP
+	// Configuring CORS for both development and production environments
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000, http://127.0.0.1:3000,  http://go-backend:3000",
+		AllowOrigins:     "http://localhost:3000, http://127.0.0.1:3000, http://34.32.62.23, http://34.32.62.23:80",
+		AllowMethods:     "GET, POST, HEAD, PUT, DELETE, PATCH",
 		AllowHeaders:     "Origin, Content-Type, Accept",
 		AllowCredentials: true,
 	}))
