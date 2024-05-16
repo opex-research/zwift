7e1ea021e7f5a573bc08673d1517a252503f46ce
Michel Osswald <ga27caf@mytum.de>
2024-05-16 13:29:54 +0200

    - fix

--- Backend/router/router.go
+++ Backend/router/router.go
@@ -4,22 +4,31 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/session"
 	handlers "github.com/opex-research/zwift/handlers"
 )
+
+// CustomCORS middleware to allow all origins while allowing credentials
+func CustomCORS() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		origin := c.Get("Origin")
+		c.Set("Access-Control-Allow-Origin", origin)
+		c.Set("Access-Control-Allow-Methods", "GET,POST,HEAD,PUT,DELETE,PATCH")
+		c.Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
+		c.Set("Access-Control-Allow-Credentials", "true")
+		if c.Method() == "OPTIONS" {
+			return c.SendStatus(fiber.StatusNoContent)
+		}
+		return c.Next()
+	}
+}
 
 // SetupRoutes configures the application routes
 func SetupRoutes(app *fiber.App, store *session.Store) {
 	h := &handlers.Handler{Store: store}
 
-	// Configuring CORS to be completely open for debugging
-	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "*",
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
-		AllowHeaders:     "Origin,Content-Type,Accept",
-		AllowCredentials: true,
-	}))
+	// Use the custom CORS middleware
+	app.Use(CustomCORS())
 
 	// Middleware to log requests
 	app.Use(func(c *fiber.Ctx) error {
